9b0020f85976b93ede169f40349c2de0be21ef17
Tavella Laurène <laurene.tavella@bbox.fr>
2018-10-14 20:23:56 +0200

    moodUI follow character
    The moodText follows the character where he goes, and its size is controlled.

--- ecm/Assets/ECM/Scripts/UI/CharacterMood.cs
+++ ecm/Assets/ECM/Scripts/UI/CharacterMood.cs
@@ -6,20 +6,26 @@ using UnityEngine.UI;
 public class CharacterMood : MonoBehaviour {
 
     private Character character;
-    public GameObject moodUi;
+    public RectTransform moodUi;
     public Transform canvas;
     private Text moodText;
+    private Camera cam;
+    
+
 
 	// Use this for initialization
 	void Start () {
+        cam = Camera.main;
         character = GetComponent<Character>();
-        GameObject moodInstance = Instantiate(moodUi, canvas);
-        moodText = moodInstance.GetComponentInChildren<Text>();
+        moodUi = Instantiate(moodUi.gameObject, canvas).GetComponent<RectTransform>();
+        moodText = moodUi.GetComponentInChildren<Text>();
         moodText.text = character.mood.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        moodUi.transform.position = cam.WorldToScreenPoint(transform.position + Vector3.up * 2.1f);
+        moodUi.transform.localScale = Vector3.one * Mathf.Min(1, 30 / cam.fieldOfView);
+        //moodUi.transform.position = new Vector2(Random.value, Random.value);
+    }
 }
