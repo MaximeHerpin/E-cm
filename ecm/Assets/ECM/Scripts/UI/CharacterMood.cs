e88898d05bcf317b5d16bc5162dc262535c59c63
Tavella Laurène <laurene.tavella@bbox.fr>
2018-10-14 18:24:40 +0200

    Mood UI
    The mood of a personnage is diplayed in a canvas 2D.

---
+++ ecm/Assets/ECM/Scripts/UI/CharacterMood.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMood : MonoBehaviour {
+
+    private Character character;
+    public GameObject moodUi;
+    public Transform canvas;
+    private Text moodText;
+
+	// Use this for initialization
+	void Start () {
+        character = GetComponent<Character>();
+        GameObject moodInstance = Instantiate(moodUi, canvas);
+        moodText = moodInstance.GetComponentInChildren<Text>();
+        moodText.text = character.mood.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
