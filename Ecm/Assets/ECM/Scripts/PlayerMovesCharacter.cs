157d5238ace6ef289930dcdc7ad79041559dac3a
IsabelleLePivain <isabelle.lepivain@centrale-marseille.fr>
2018-11-27 16:34:47 +0100

    #13 A Système de déplacement guidé d'un personage
    A. Un personnage contrôlable doit pouvoir être déplacé sur toute la map à l'aide des touches du clavier.
    + Caméra suivant le personnage

--- Ecm/Assets/ECM/Scripts/PlayerMovesCharacter.cs
+++ Ecm/Assets/ECM/Scripts/PlayerMovesCharacter.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovesCharacter : MonoBehaviour {
+    private NavMeshAgent navMeshAgent;
+    private float characterSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        //characterSpeed = gameObject.GetComponent<CARAC.Speed>(); // Avec propriétés des persos sur script de Maxime
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float forward = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Horizontal");
+        Vector3 deplacement = new Vector3(turn/2, 0, forward/2);
+        //Vector3 deplacement = new Vector3(turn*characterSpeed, 0, forward*characterSpeed);
+        navMeshAgent.Move(deplacement);
+
+
+    }
 }
