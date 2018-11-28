8e3e44e54c1b716d10cdd9d535e858700b314ff3
Jordan Talobre <jordan.talobre@centrale-marseille.fr>
2018-11-28 16:24:05 +0100

    Queue
    Creation queue in order to go to the RU

---
+++ Ecm/Assets/ECM/Scripts/FSMBehaviours/QueueRU.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class QueueAndEat : StateMachineBehaviour
+{
+
+    NavMeshAgent agent;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.gameObject.GetComponent<NavMeshAgent>();
+        Vector3 destination;
+        Vector3 position = animator.transform.position;
+
+        destination = FindQueue(position);
+        agent.SetDestination(destination);
+        animator.ResetTrigger("DestinationReached");
+    }
+
+
+
+    private Vector3 FindQueue(Vector3 position)
+    {
+        return GameObject.FindGameObjectWithTag("Queue").GetComponent<ClassRoom>().GetNextPosition();
+    }
+
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
